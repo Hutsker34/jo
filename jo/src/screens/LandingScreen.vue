a411b4e27d30dc4998fd15767cb1ce2911d0ce80
viacheslav34 <77201329+viacheslav34@users.noreply.github.com>
2022-08-25 18:53:19 +0300

    верстаю

--- jo/src/screens/LandingScreen.vue
+++ jo/src/screens/LandingScreen.vue
@@ -30,7 +30,7 @@
         <h1 class='steps__h1'>этапы разработки - от оформления заказа до получения рабочего сайта </h1>
         <div class='steps__cards'>
             <div class='cards__card'>
-                <p claass='card__num'>|</p>
+                <p class='card__num'>|</p>
                 <div class='card__wrap'>
                     <img src='../assets/steps__img1.png' class='card__img card__img--1'/>
                     <h2 class='card__h2'>оформление закака</h2>
@@ -43,7 +43,7 @@
             </div>
             <div class='steps__arrow'>=></div>
             <div class='steps__card'>
-                <p claass='card__num'>||</p>
+                <p class='card__num'>||</p>
                 <div class='card__wrap'>
                     <img src='../assets/steps__img2.png' class='card__img'/>
                     <h2 class='card__h2'>разработка </h2>
@@ -56,7 +56,7 @@
             </div>
             <div class='steps__arrow'>=></div>
             <div class='steps__card'>
-                <p claass='card__num'>sss</p>
+                <p class='card__num'>|||</p>
                 <div class='card__wrap'>
                     <img src='../assets/steps__img3.png' class='card__img'/>
                     <h2 class='card__h2'>представление готового проекта</h2>
@@ -69,7 +69,21 @@
             </div>
         </div>
     </div>
-    <div class='form'></div>
+    <div class='order'>
+        <h1 class='order__h1'>Оформление заказа</h1>
+        <div class='order__wrap'>
+            <img src='../assets/form__img.png' class='order__img'/>
+            <form class='order__form'>
+                <label class='form__label' >как вас зовут?</label>
+                <input class='form__input' type='text'>
+                 <label class='form__label'>укажите ваш номер телефона</label>
+                <input class='form__input' type='text'>
+                 <label class='form__label ' >расскажите о своём проекте</label>
+                <textarea class='form__input form__label--big' ></textarea>
+                <button class='form__button'>оформить</button>
+            </form>
+        </div>
+    </div>
     <footer></footer>
 </template>
 <script>
@@ -147,7 +161,7 @@ data() {
     .steps__cards{
         display: flex;
         justify-content: space-between;
-        margin: 170px 0 0 0;
+        margin: 130px 0 0 0;
     }
     .steps{
         margin-top: 250px;
@@ -159,6 +173,7 @@ data() {
         border-radius: 5px;
         padding: 10px;
         text-align: start;
+        min-height: 450px;
     }
     .card__h2{
         font-size: 20px;
@@ -188,5 +203,61 @@ data() {
     }
     .card__num{
         font-size: 32px;
+        color: #79A7D3;
     }
+    .steps__arrow{
+        margin-top: 23%;
+        font-size: 32px;
+        color: #8A307F;
+    }
+    .order__wrap{
+        display: flex;
+        justify-content: space-between;
+    }
+    .order{
+        margin-top: 250px;
+        text-align: center;
+        padding: 30px 200px 0 200px;
+    }
+    .order__form{
+        display: flex;
+        flex-direction: column;
+        text-align: start;
+        margin: 150px 150px 0 0;
+        align-items: flex-start
+    }
+    .form__input{
+        background: #79A7D3;
+        border: none;
+        border-radius: 4px;
+        width:400px;
+        font-size:22px;
+        padding: 5px 20px 5px 20px;
+        
+   }
+    .form__label--big{
+       min-width:600px;
+       min-height: 150px;
+       font-size: 12px;
+        padding-top: 10px;
+    }
+    .order__h1{
+        color: #234895;
+    }
+    .form__label{
+        font-size: 22px;
+    }
+    .form__button{
+        background: #8A307F;
+        border-radius: 30px;
+        border:none;
+        color: white;
+        padding: 10px 40px 10px 40px;
+        font-size:20px;
+        font-weight: thin;
+        margin: 30px 0 0 470px;
+        min-width: 150px;
+        color: white;
+    }
+
 </style>
